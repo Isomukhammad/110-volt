38d4284aa9873d11d4d8bdf607f1e73bff88bb68
Bilol Tuliboev <tuliboevbilol@gmail.com>
2022-12-29 18:15:08 +0500

    feat: multi column brand slider

--- src/components/BrandTab/BrandTap.jsx
+++ src/components/BrandTab/BrandTap.jsx
@@ -1,14 +1,21 @@
-import Image from 'next/image';
-import Link from 'next/link';
-import styles from './BrandTap.module.scss';
+import Image from 'next/image'
+import Link from 'next/link'
+import styles from './BrandTap.module.scss'
 
 const BrandTab = ({ info }) => {
-    const { img, name } = info;
-    return (
-        <Link href="/" className={styles.tab}>
-            <Image src={img} alt={name} width="0" height="0" sizes="100vw" className={styles.image} />
-        </Link>
-    )
+	const { img, name } = info
+	return (
+		<Link href='/' className={styles.tab}>
+			<Image
+				src={img}
+				alt={name}
+				width='0'
+				height='0'
+				sizes='100vw'
+				className={styles.image}
+			/>
+		</Link>
+	)
 }
 
-export default BrandTab;
+export default BrandTab
