810f1be5f4894dc1a854ba0a0fe3fba740b50f60
Isomukhammad <s.isomuxammad@gmail.com>
2023-01-01 23:48:06 +0500

    fix: checking if coundown is positive

--- src/utils/getCoundown.js
+++ src/utils/getCoundown.js
@@ -9,15 +9,22 @@ const getCountdown = (time) => {
     const current = Date.now();
     const countdown = timestamp - current;
 
-    const days = Math.floor(countdown / (1000 * 60 * 60 * 24));
-    const hours = Math.floor((countdown % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    const minutes = Math.floor((countdown % (1000 * 60 * 60)) / (1000 * 60));
-    const seconds = Math.floor((countdown % (1000 * 60)) / 1000);
+    if (countdown >= 0) {
+        const days = Math.floor(countdown / (1000 * 60 * 60 * 24));
+        const hours = Math.floor((countdown % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        const minutes = Math.floor((countdown % (1000 * 60 * 60)) / (1000 * 60));
+        const seconds = Math.floor((countdown % (1000 * 60)) / 1000);
+        left.days = days;
+        left.hours = hours;
+        left.minutes = minutes;
+        left.seconds = seconds;
+    } else {
+        left.days = 0;
+        left.hours = 0;
+        left.minutes = 0;
+        left.seconds = 0;
+    }
 
-    left.days = days;
-    left.hours = hours;
-    left.minutes = minutes;
-    left.seconds = seconds;
 
     return left;
 }
