0d212a63fd3a9dc887913d94d1c3621de0ab55df
Isomukhammad <s.isomuxammad@gmail.com>
2023-01-05 00:49:32 +0500

    feat: adding offer affrement

--- src/components/CartTotal/CartTotal.jsx
+++ src/components/CartTotal/CartTotal.jsx
@@ -1,7 +1,11 @@
+import Link from 'next/link';
+import { useRouter } from 'next/router';
 import Button from '../Button/Button';
 import styles from './CartTotal.module.scss'
 
-const CartTotal = () => {
+const CartTotal = ({ offer }) => {
+    const router = useRouter();
+
     return (
         <div className={styles.container}>
             <div className={styles.content}>
@@ -11,9 +15,27 @@ const CartTotal = () => {
                 </div>
                 <h2 className={styles.price}>76 266 000 сум</h2>
                 <div className={styles.buttons}>
-                    <Button>Перейти к оформлению</Button>
+                    <Link href="/checkout">
+                        <Button>Перейти к оформлению</Button>
+                    </Link>
                     <Button type="reverse">Купить в рассрочку</Button>
                 </div>
+                {
+                    offer ? <div className={styles.offer}>
+                        <svg
+                            viewBox='0 0 16 17'
+                            width={16}
+                            height={17}
+                            fill="none"
+                            stroke="#7B54C9"
+                        >
+                            <use xlinkHref='#tick-logo'></use>
+                        </svg>
+                        <p>
+                            Нажимая на кнопку “Оформить заказ” вы соглашаетесь c условиями <span onClick={() => router.push('/offer')}>Оферты</span>
+                        </p>
+                    </div> : null
+                }
             </div>
             <Button type="cart" active={true}>Добавить всё в избранное</Button>
             <Button type="cart">Очистить корзину</Button>
