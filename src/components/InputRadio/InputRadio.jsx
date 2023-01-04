e64e35e758f039d510e37d87f137f8adefd9586f
Isomukhammad <s.isomuxammad@gmail.com>
2023-01-05 00:50:50 +0500

    feat: adding styling of button

--- src/components/InputRadio/InputRadio.jsx
+++ src/components/InputRadio/InputRadio.jsx
@@ -1,15 +1,23 @@
 import styles from './InputRadio.module.scss';
 
-const InputRadio = ({ name, color, value }) => (
-    <div className={styles.container}>
-        <input
-            type="radio"
-            name={name}
-            value={value}
-            style={{ backgroundColor: { color } }}
-            className={styles.radio}
-        />
-    </div>
-)
+const InputRadio = ({ name, color, value }) => {
+    const red = color === "red" ? styles.red : '';
+    const black = color === "black" ? styles.black : '';
+    const white = color === "white" ? styles.white : '';
+    const purple = color === "purple" ? styles.purple : '';
+    const orange = color === "orange" ? styles.orange : '';
+    const blue = color === "blue" ? styles.blue : '';
+
+    return (
+        <div className={`${styles.container} ${red} ${black} ${white} ${purple} ${orange} ${blue}`}>
+            <input
+                type="radio"
+                name={name}
+                value={value}
+                className={styles.radio}
+            />
+        </div>
+    )
+}
 
 export default InputRadio;
