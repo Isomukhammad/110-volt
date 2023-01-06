b5584e7de6c089db97409b95873f1cc6e410283b
Isomukhammad <s.isomuxammad@gmail.com>
2023-01-06 18:39:54 +0500

    feat: adapting for mobile and tablets

--- src/pages/checkout/index.js
+++ src/pages/checkout/index.js
@@ -1,16 +1,23 @@
+import { useContext, useState } from 'react';
+
+import HeadInfo from '../../utils/HeadInfo';
+import { ScreenSize } from '../../context/screenContext'
+
+import products from '../../products.json';
+
 import CartError from '../../components/CartError/CartError';
 import CartItem from '../../components/CartItem/CartItem';
 import DiscountTabs from '../../components/DiscountTabs/DiscountTabs';
 import PagePath from '../../components/PagePath/PagePath';
-import HeadInfo from '../../utils/HeadInfo';
-
-import products from '../../products.json';
-
-import styles from './Checkout.module.scss';
 import CartTotal from '../../components/CartTotal/CartTotal';
 import PersonalInfo from '../../components/PersonalInfo/PersonalInfo';
 
+import styles from './Checkout.module.scss';
+import PopUp from '../../components/PopUp/PopUp';
+
 const CheckoutPage = () => {
+    const { isMobile } = useContext(ScreenSize)
+    const [popUp, setPopUp] = useState(true)
     return (
         <>
             <HeadInfo title="Оформить заказ" />
@@ -33,31 +40,40 @@ const CheckoutPage = () => {
             <div className={styles.container}>
                 {
                     products ? (
-                        <div className={styles.content}>
-                            <h1 className={styles.title}>Корзина</h1>
+                        <>
+                            {
+                                !isMobile ? (
+                                    <div className={styles.content}>
+                                        <h1 className={styles.title}>Корзина</h1>
 
-                            <div className={styles.cart}>
-                                <div className={styles.cartItems}>
-                                    <CartItem info={products[0].products[0]} checkout={true} />
-                                    <CartItem info={products[0].products[0]} checkout={true} />
-                                    <CartItem info={products[0].products[0]} checkout={true} />
-                                    <hr />
-                                    <div className={styles.amount}>
-                                        <div></div>
-                                        <div>Итого:</div>
-                                        <div>63 114 300 сум</div>
+                                        <div className={styles.cart}>
+                                            <div className={styles.cartItems}>
+                                                <CartItem info={products[0].products[0]} checkout={true} />
+                                                <CartItem info={products[0].products[0]} checkout={true} />
+                                                <CartItem info={products[0].products[0]} checkout={true} />
+                                                <hr />
+                                                <div className={styles.amount}>
+                                                    <div></div>
+                                                    <div>Итого:</div>
+                                                    <div>63 114 300 сум</div>
+                                                </div>
+                                            </div>
+                                            <div className={styles.cartTotal}>
+                                                <CartTotal offer={true} />
+                                            </div>
+                                        </div>
                                     </div>
-                                </div>
-                                <div className={styles.cartTotal}>
-                                    <CartTotal offer={true} />
-                                </div>
-                            </div>
-                        </div>
+                                ) : null
+                            }
+                        </>
                     ) : (
                         <CartError />
                     )
                 }
                 <PersonalInfo />
+                {
+                    popUp ? <PopUp result='success' setPopUp={setPopUp} /> : null
+                }
                 <DiscountTabs />
             </div>
         </>
