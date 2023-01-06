fabff31bde90bbe20efe4e96dc673fb695e6525b
Isomukhammad <s.isomuxammad@gmail.com>
2023-01-06 18:38:15 +0500

    feat: adding state for tablet width

--- src/context/screenContext.js
+++ src/context/screenContext.js
@@ -3,14 +3,18 @@ import { createContext, useEffect, useState } from "react";
 export const ScreenSize = createContext(null);
 
 const ScreenContext = ({ children }) => {
-    const [isMobile, setIsMobile] = useState(false)
+    const [isMobile, setIsMobile] = useState(false);
+    const [isTablet, setIsTablet] = useState(false);
 
     //choose the screen size 
     const handleResize = () => {
         if (window.innerWidth <= 768) {
             setIsMobile(true)
+        } else if (window.innerWidth > 768 && window.innerWidth <= 1024) {
+            setIsTablet(true)
         } else {
             setIsMobile(false)
+            setIsTablet(false)
         }
     }
 
@@ -22,10 +26,19 @@ const ScreenContext = ({ children }) => {
         if (window.innerWidth <= 768) {
             setIsMobile(true);
         }
+
+        if (window.innerWidth > 768 && window.innerWidth <= 1024) {
+            setIsTablet(true)
+        }
     }, [])
 
+    const values = {
+        isMobile,
+        isTablet
+    }
+
     return (
-        <ScreenSize.Provider value={isMobile}>
+        <ScreenSize.Provider value={values}>
             {children}
         </ScreenSize.Provider>
     )
