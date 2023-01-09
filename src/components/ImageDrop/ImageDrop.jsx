6134e530997fda9d362353c186d8fbec71e3b3ac
Isomukhammad <s.isomuxammad@gmail.com>
2023-01-09 15:23:32 +0500

    feat: image dropzone component

---
+++ src/components/ImageDrop/ImageDrop.jsx
@@ -0,0 +1,47 @@
+import { useCallback } from "react";
+import Dropzone, { useDropzone } from "react-dropzone";
+import ImageComponent from "../ImageComponent/ImageComponent";
+
+import styles from './ImageDrop.module.scss'
+
+const ImageDrop = ({ image }) => {
+    const onDrop = useCallback(acceptedFiles => {
+        console.log(acceptedFiles)
+    }, [])
+
+    const {
+        acceptedFiles,
+        fileRejections,
+        getRootProps,
+        getInputProps,
+        isDragActive
+    } = useDropzone({
+        onDrop,
+        accept: {
+            'image/jpeg': [],
+            'image/png': []
+        },
+        maxFiles: 2
+    })
+
+    const acceptedFileItems = acceptedFiles.map(file => (
+        <li key={file.path}>
+            {file.path} - {file.size} байт
+        </li>
+    ));
+
+    return (
+        <div className={styles.container}>
+            <ImageComponent src={image} alt="" />
+            <div className={styles.description}>
+                <p> <span {...getRootProps()} className={styles.input}>Нажмите на ссылку</span>, чтобы выбрать фотографии или просто перетащите их сюда</p>
+            </div>
+
+            <aside>
+                {acceptedFileItems}
+            </aside>
+        </div>
+    );
+}
+
+export default ImageDrop;
