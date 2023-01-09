0eae26dd662dee9f151c06557c2cb0b9e21fd2ff
Isomukhammad <s.isomuxammad@gmail.com>
2023-01-09 15:42:41 +0500

    fix: changing input

--- src/components/ImageDrop/ImageDrop.jsx
+++ src/components/ImageDrop/ImageDrop.jsx
@@ -33,8 +33,8 @@ const ImageDrop = ({ image }) => {
     return (
         <div className={styles.container}>
             <ImageComponent src={image} alt="" />
-            <div className={styles.description} {...getRootProps()}>
-                <p> <input {...getInputProps()} className={styles.input} />, чтобы выбрать фотографии или просто перетащите их сюда</p>
+            <div className={styles.description}>
+                <p><span className={styles.input} {...getRootProps()}><input {...getInputProps()} />Нажмите на ссылку</span>, чтобы выбрать фотографии или просто перетащите их сюда</p>
             </div>
 
             <aside>
