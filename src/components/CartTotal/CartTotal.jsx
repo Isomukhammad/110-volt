96d6df52dc5ff43c3de45c5da8093e0791846d56
Isomukhammad <s.isomuxammad@gmail.com>
2023-01-10 23:04:16 +0500

    fix: adding mobile version

--- src/components/CartTotal/CartTotal.jsx
+++ src/components/CartTotal/CartTotal.jsx
@@ -1,72 +1,93 @@
 import Link from 'next/link';
 import { useRouter } from 'next/router';
+import { useContext, useEffect } from 'react';
+import { ScreenContext } from '../../context/screenContext';
 import Button from '../Button/Button';
 import styles from './CartTotal.module.scss'
 
 const CartTotal = ({ offer }) => {
     const router = useRouter();
-    console.log(router.pathname)
+    const { isMobile, isTablet } = useContext(ScreenContext)
 
-    return (
-        <div className={styles.container}>
-            <div className={styles.content}>
-                <div className={styles.quantity}>
-                    <h3>В корзине</h3>
-                    <p>3 товара</p>
+    useEffect(() => {
+        console.log(isTablet);
+        console.log(isMobile)
+    }, [isMobile, isTablet])
+
+    if (isMobile) {
+        return (
+            <div className={styles.bottomTotal}>
+                <div className={styles.content}>
+                    <div className={styles.prices}>
+                        <p>3 товара</p>
+                        <h4>75 266 000 cум</h4>
+                    </div>
+                    <Button>Заказать</Button>
                 </div>
-                <h2 className={styles.price}>76 266 000 сум</h2>
-                <div className={styles.actionButtons}>
+            </div>
+        )
+    } else {
+        return (
+            <div className={styles.totalContainer}>
+                <div className={styles.content}>
+                    <div className={styles.quantity}>
+                        <h3>В корзине</h3>
+                        <p>3 товара</p>
+                    </div>
+                    <h2 className={styles.price}>76 266 000 сум</h2>
+                    <div className={styles.actionButtons}>
+                        {
+                            router.pathname === '/checkout' ? (
+                                <>
+                                    <Link href="/checkout">
+                                        <Button>Оформить заказ</Button>
+                                    </Link>
+                                    <Link href="/cart">
+                                        <Button type="reverse">Назад</Button>
+                                    </Link>
+                                </>
+                            ) : (
+                                <>
+                                    <Link href="/checkout">
+                                        <Button>Перейти к оформлению</Button>
+                                    </Link>
+                                    <Button type="reverse">Купить в рассрочку</Button>
+                                </>
+                            )
+                        }
+                    </div>
                     {
-                        router.pathname === '/checkout' ? (
-                            <>
-                                <Link href="/checkout">
-                                    <Button>Оформить заказ</Button>
-                                </Link>
-                                <Link href="/cart">
-                                    <Button type="reverse">Назад</Button>
-                                </Link>
-                            </>
-                        ) : (
-                            <>
-                                <Link href="/checkout">
-                                    <Button>Перейти к оформлению</Button>
-                                </Link>
-                                <Button type="reverse">Купить в рассрочку</Button>
-                            </>
-                        )
+                        offer ? <div className={styles.offer}>
+                            <svg
+                                viewBox='0 0 16 17'
+                                width={16}
+                                height={17}
+                                fill="none"
+                                stroke="#7B54C9"
+                            >
+                                <use xlinkHref='#tick-logo'></use>
+                            </svg>
+                            <p>
+                                Нажимая на кнопку “Оформить заказ” вы соглашаетесь c условиями <span onClick={() => router.push('/offer')}>Оферты</span>
+                            </p>
+                        </div> : null
                     }
                 </div>
                 {
-                    offer ? <div className={styles.offer}>
-                        <svg
-                            viewBox='0 0 16 17'
-                            width={16}
-                            height={17}
-                            fill="none"
-                            stroke="#7B54C9"
-                        >
-                            <use xlinkHref='#tick-logo'></use>
-                        </svg>
-                        <p>
-                            Нажимая на кнопку “Оформить заказ” вы соглашаетесь c условиями <span onClick={() => router.push('/offer')}>Оферты</span>
-                        </p>
-                    </div> : null
+                    router.pathname === '/checkout' ? (
+                        null
+                    ) : (
+                        <>
+                            <div className={styles.cartButtons}>
+                                <Button type="cart" active={true}>Добавить всё в избранное</Button>
+                                <Button type="cart">Очистить корзину</Button>
+                            </div>
+                        </>
+                    )
                 }
             </div>
-            {
-                router.pathname === '/checkout' ? (
-                    null
-                ) : (
-                    <>
-                        <div className={styles.cartButtons}>
-                            <Button type="cart" active={true}>Добавить всё в избранное</Button>
-                            <Button type="cart">Очистить корзину</Button>
-                        </div>
-                    </>
-                )
-            }
-        </div>
-    );
+        )
+    }
 }
 
 export default CartTotal;
