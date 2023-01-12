c1c7e90616bf6caa430c1ebd5a1937e959bb3444
Isomukhammad <s.isomuxammad@gmail.com>
2023-01-12 11:04:57 +0500

    feat: price animation

--- src/components/CategoryItem/CategoryItem.jsx
+++ src/components/CategoryItem/CategoryItem.jsx
@@ -1,18 +1,35 @@
 import Image from "next/image";
 import Link from "next/link";
+import { useState } from "react";
 
 import styles from './CategoryItem.module.scss';
 
 const CategoryItem = ({ info }) => {
     const { img, price, discounted, monthly, name } = info;
+    const [width, setWidth] = useState(100);
+
+    const increaseWidth = () => {
+        setWidth(0)
+    }
+
+    const decreaseWidth = () => {
+        setWidth(-100);
+    }
 
     return (
-        <div className={styles.tab}>
+        <div
+            className={styles.tab}
+            onMouseEnter={increaseWidth}
+            onMouseLeave={decreaseWidth}
+        >
             <div href='/' className={styles.image}>
                 <Image src={img} alt={name} sizes="100vw" width="0" height="0" placeholder="blurDataURL" />
             </div>
             <div className={styles.monthly}>
-                <div className={styles.background}></div>
+                <div
+                    className={styles.background}
+                    style={{ right: `${width}%` }}
+                ></div>
                 <p>{monthly} сум/мес</p>
             </div>
             <div className={styles.name}>{name}</div>
