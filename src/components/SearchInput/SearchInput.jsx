9fd3e83c0ff851ae0c2aedade53b87e21e3a4d74
Isomukhammad <s.isomuxammad@gmail.com>
2023-02-03 23:10:30 +0500

    fix: input styling

--- src/components/SearchInput/SearchInput.jsx
+++ src/components/SearchInput/SearchInput.jsx
@@ -2,7 +2,7 @@ import styles from './SearchInput.module.scss'
 
 const SearchInput = ({ placeholder, onChange }) => {
     return (
-        <input type="text" placeholder={placeholder} onChange={onChange} className={styles.search} />
+        <input type="text" placeholder={placeholder} onChange={onChange} className="block py-4 px-[14px] w-full text-[15px] text-white bg-[#ffffff33] rounded-[16px] border-none appearance-none focus:outline-none focus:ring-0 focus:border-transparent peer placeholder:text-[#ffffff80]" />
     )
 }
 
