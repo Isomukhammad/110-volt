4ddc1511744fc183cc9143a413713e1b642564f9
Isomukhammad <s.isomuxammad@gmail.com>
2023-02-03 23:12:11 +0500

    feat: adding context for common datas

--- src/context/dataContext.js
+++ src/context/dataContext.js
@@ -1,29 +1,48 @@
 import { useRouter } from "next/router";
+import { useContext } from "react";
 import { createContext } from "react";
 import useSWR from "swr";
-import { fetcher } from "../utils/fetcher";
+import fetcher from "../utils/fetcher";
 
-export const DataContext = createContext();
+const DataContext = createContext();
 
-const DataProvider = ({ children }) => {
+const disableRevalidation = {
+    revalidateOnFocus: false,
+    revalidateOnReconnect: false,
+    refreshWhenHidden: 0,
+    refreshWhenHidden: false,
+    refreshWhenOffline: false,
+}
+
+export const DataProvider = ({ children }) => {
     const router = useRouter();
 
     const {
-        data: tree,
-        error: treeError,
-        mutate: mutateTree,
-    } = useSWR("https://jsonplaceholder.typicode.com/todos/", fetcher);
+        data: settings,
+        error: settingsError,
+        isValidating: settingsVal
+    } = useSWR(
+        '/settings',
+        (url) => fetcher(url),
+        disableRevalidation
+    )
 
-    const values = {
-        tree,
-        treeError
-    }
+    const {
+        data: menu,
+        error: menuError,
+        isValidating: menuValidating
+    } = useSWR(
+        '/categories/tree', (url) => fetcher(url),
+        disableRevalidation
+    );
 
     return (
-        <DataContext.Provider value={values}>
+        <DataContext.Provider
+            value={{ settings, settingsError, settingsVal, menu, menuError, menuValidating }}
+        >
             {children}
         </DataContext.Provider>
     )
 }
 
-export default DataProvider;
+export const useData = () => useContext(DataContext);
