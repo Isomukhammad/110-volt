aea346f7394430bde327472465d696917b331583
Isomukhammad <s.isomuxammad@gmail.com>
2023-02-03 23:07:06 +0500

    feat: connecting to API

--- src/components/PopularCategories/PopularCategories.jsx
+++ src/components/PopularCategories/PopularCategories.jsx
@@ -1,16 +1,27 @@
-import data from '../../data.json'
+import useSWR from 'swr';
+import fetcher from '../../utils/fetcher';
 import PopularCategoryTab from '../PopularCategoryTab/PopularCategoryTab'
+
 import styles from './PopularCategories.module.scss';
 
 const PopularCategories = () => {
+    const { data: categories, error, isValidating, mutate } = useSWR('/categories', (url) => fetcher(url),
+        {
+            revalidateIfStale: false,
+            revalidateOnFocus: false,
+            revalidateOnReconnect: false,
+        }
+    )
+
     return (
         <div className={styles.popularCategories}>
-            <h2>Популярные категории</h2>
+            <h2 className='font-bold text-[24px] lg:text-[32px]'>Популярные категории</h2>
             <div className={styles.tabs}>
-                {
-                    data.popularCategories.map((item) => (
-                        <PopularCategoryTab key={item.id} info={item} />
+                {!isValidating ? (
+                    categories.data.map((item) => (
+                        <PopularCategoryTab key={item.id} data={item} />
                     ))
+                ) : (null)
                 }
             </div>
         </div>
