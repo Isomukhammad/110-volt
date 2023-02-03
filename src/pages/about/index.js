ee07990c2fab9ba27560c879b504242a68f29b62
Isomukhammad <s.isomuxammad@gmail.com>
2023-02-03 23:18:30 +0500

    feat: fixing default tailwind stylings

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -1,5 +1,4 @@
 import { useRouter } from "next/router";
-import AboutDescription from "../../components/AboutDescription/AboutDescription";
 import DiscountTabs from "../../components/DiscountTabs/DiscountTabs";
 import PagePath from "../../components/PagePath/PagePath";
 import HeadInfo from "../../utils/HeadInfo";
@@ -24,7 +23,31 @@ const AboutPage = () => {
                     }
                 ]}
             />
-            <AboutDescription />
+            <div className={styles.content}>
+                <h1 className="font-bold text-[24px] md:text-[32px]">О нашей компании</h1>
+
+                <div className={styles.description}>
+                    <p>
+                        110volt – это инновационный интернет-магазин, который ориентирован на решение проблем потребителей связанных с покупками бытовых предметов и современной электроники. Каждый день наша команда работает над вопросом «что сделать что бы покупателю было максимально удобно».
+                    </p>
+
+                    <p>
+                        В нашем онлайн-магазине вы найдёте:
+                    </p>
+
+                    <ul>
+                        <li>Телефоны и Гаджеты;</li>
+                        <li>Крупную и мелкую бытовую технику;</li>
+                        <li>Товары красоты и здоровья;</li>
+                        <li>Спортинвентарь;</li>
+                        <li>Посуду, мебель и многое другое;</li>
+                    </ul>
+
+                    <p>
+                        Покупки у нас на сайте одно удовольствие так как мы принимаем любой вид оплаты и даже оформим Ваш заказ в РАССРОЧКУ. А наш вежливый персонал круглосуточного call-центра всегда готов дать квалифицированную консультацию.
+                    </p>
+                </div>
+            </div>
             <DiscountTabs />
         </div>
     )
