79460368e3eee3ea089d5a015160f95f2d7f1f4e
Isomukhammad <s.isomuxammad@gmail.com>
2023-02-03 23:05:47 +0500

    refactor: moving to another folder

---
+++ src/components/News/NewsTab.jsx
@@ -0,0 +1,28 @@
+import Link from 'next/link';
+
+import ImageComponent from '../ImageComponent/ImageComponent';
+
+import styles from './NewsTab.module.scss'
+
+const NewsTab = ({ data }) => {
+    const { img, name, description, url, id, slug } = data;
+    return (
+        <Link href={`/news/${id}-${slug}`} className={styles.container}>
+            <div className={styles.image}>
+                <ImageComponent
+                    src={img}
+                    alt="Изображение новости"
+                    sizes='100vw'
+                    width={0}
+                    height={0}
+                />
+            </div>
+            <h3>{name}</h3>
+            <div>
+                <p>{description}</p>
+            </div>
+        </Link>
+    )
+}
+
+export default NewsTab;
---
+++ src/components/News/NewsTabs.jsx
@@ -0,0 +1,22 @@
+import news from '../../news.json';
+
+import NewsTab from './NewsTab'
+
+import styles from './NewsTabs.module.scss';
+
+const NewsTabs = ({ data }) => {
+    return (
+        <div className={styles.container}>
+            {
+                data.data.map((item) => (
+                    <NewsTab
+                        key={item.id}
+                        data={item}
+                    />
+                ))
+            }
+        </div>
+    )
+}
+
+export default NewsTabs; 
