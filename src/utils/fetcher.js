acb17f9c1c2e67fd91789132ae96fdd74e7dd108
Isomukhammad <s.isomuxammad@gmail.com>
2023-02-03 23:22:40 +0500

    feat: fetcher for useSWR

--- src/utils/fetcher.js
+++ src/utils/fetcher.js
@@ -1,3 +1,18 @@
-const fetcher = (url) => fetch(url).then((res) => res.json());
+const updateOptions = (options) => {
+    const update = {
+        ...options,
+        headers: {
+            ...options.headers,
+            Accept: 'application/json',
+        },
+    }
+
+    return update
+}
+
+const fetcher = async (url = '', options = {}) => {
+    const __url = new URL(process.env.API + url);
+    return fetch(__url, updateOptions(options)).then((res) => res.json())
+};
 
 export default fetcher;
