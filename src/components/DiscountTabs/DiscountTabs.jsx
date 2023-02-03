06b626e9976fcd180d6affc8273ef08ea8bf658a
Isomukhammad <s.isomuxammad@gmail.com>
2023-02-03 23:00:12 +0500

    feat: connecting to API

--- src/components/DiscountTabs/DiscountTabs.jsx
+++ src/components/DiscountTabs/DiscountTabs.jsx
@@ -10,87 +10,96 @@ import "swiper/css/navigation";
 import "swiper/css/free-mode";
 import "swiper/css/thumbs";
 
-import { Pagination, Navigation, Autoplay } from "swiper";
+import { Pagination, Navigation, Autoplay, FreeMode } from "swiper";
 
 import styles from './DiscountTabs.module.scss'
 import Image from "next/image";
+import useSWR from "swr";
+import fetcher from "../../utils/fetcher";
+import Link from "next/link";
 
 const DiscountTabs = () => {
+    const { data, error, isValidating, mutate } = useSWR('/banners?type=home_block_3_slide', (url) => fetcher(url),
+        {
+            revalidateIfStale: false,
+            revalidateOnFocus: false,
+            revalidateOnReconnect: false,
+        }
+    );
     const { isMobile } = useContext(ScreenContext)
 
     const swiperRef = useRef();
 
-    if (isMobile) {
-        return (
-            <div className={styles.containerMobile}>
-                <Image src="/images/Rectangle 5.png" alt="" width="0" height="0" sizes="100vw" placeholder="blurDataURL" />
-                <Image src="/images/Rectangle 6.png" alt="" width="0" height="0" sizes="100vw" placeholder="blurDataURL" />
-                <Image src="/images/Rectangle 5.png" alt="" width="0" height="0" sizes="100vw" placeholder="blurDataURL" />
-            </div>
-        )
-    } else {
-        return (
-            <div className={styles.container}>
-                <Swiper
-                    spaceBetween={16}
-                    slidesPerView={2}
-                    loop={true}
-                    loopFillGroupWithBlank={true}
-                    autoplay={{
-                        delay: 5000,
-                        disableOnInteraction: false,
-                    }}
-                    modules={[Pagination, Navigation, Autoplay]}
+    // if (isMobile) {
+    //     return (
+    //         <>
+    //             {!isValidating ? (
+    //                 <div className={styles.containerMobile}>
+    //                     {
+    //                         data.data.map((item) => (
+    //                             <Image src={item.img} key={item.id} alt={item.description} width="0" height="0" sizes="100vw" placeholder="blurDataURL" />
+    //                         ))
+    //                     }
+    //                 </div>
+    //             ) : (null)}
+    //         </>
+    //     )
+    // } else {
+    return (
+        <>
+            {!isValidating ? (
+                <div className={styles.container}>
+                    <Swiper
+                        spaceBetween={16}
+                        slidesPerView={isMobile ? 1 : 2}
+                        loop={true}
+                        loopFillGroupWithBlank={true}
+                        autoplay={{
+                            delay: 5000,
+                            disableOnInteraction: false,
+                        }}
+                        modules={[Pagination, Navigation, Autoplay, FreeMode]}
 
-                    onBeforeInit={(swiper) => {
-                        swiperRef.current = swiper;
-                    }}
-                >
-                    <SwiperSlide className={styles.swiperSlide}>
-                        <Image src="/images/Rectangle 5.png" alt="" width="0" height="0" sizes="100vw" placeholder="blurDataURL" />
-                    </SwiperSlide>
-                    <SwiperSlide className={styles.swiperSlide}>
-                        <Image src="/images/Rectangle 6.png" alt="" width="0" height="0" sizes="100vw" placeholder="blurDataURL" />
-                    </SwiperSlide>
-                    <SwiperSlide className={styles.swiperSlide}>
-                        <Image src="/images/Rectangle 5.png" alt="" width="0" height="0" sizes="100vw" placeholder="blurDataURL" />
-                    </SwiperSlide>
-                    <SwiperSlide className={styles.swiperSlide}>
-                        <Image src="/images/Rectangle 6.png" alt="" width="0" height="0" sizes="100vw" placeholder="blurDataURL" />
-                    </SwiperSlide>
-                    <SwiperSlide className={styles.swiperSlide}>
-                        <Image src="/images/Rectangle 5.png" alt="" width="0" height="0" sizes="100vw" placeholder="blurDataURL" />
-                    </SwiperSlide>
-                    <SwiperSlide className={styles.swiperSlide}>
-                        <Image src="/images/Rectangle 6.png" alt="" width={792} height={280} />
-                    </SwiperSlide>
-                </Swiper>
+                        onBeforeInit={(swiper) => {
+                            swiperRef.current = swiper;
+                        }}
+                    >
+                        {
+                            data.data.map((item) => (
+                                <SwiperSlide className={styles.swiperSlide} key={item.id}>
+                                    <Image src={item.img} alt={item.description} width="0" height="0" sizes="100vw" placeholder="blurDataURL" />
+                                </SwiperSlide>
+                            ))
+                        }
+                    </Swiper>
 
-                {
-                    isMobile ? null : (
-                        <>
-                            <div className={styles.swiperPrev} onClick={() => {
-                                swiperRef.current?.slidePrev()
-                            }}>
-                                <svg width={18.67} height={16.33} viewBox='0 0 28 28' fill='none' stroke="white"
-                                >
-                                    <use xlinkHref={`#arrow-left`}></use>
-                                </svg>
-                            </div>
-                            <div className={styles.swiperNext} onClick={() => {
-                                swiperRef.current?.slideNext()
-                            }}>
-                                <svg width={18.67} height={16.33} viewBox='0 0 28 28' fill='none' stroke="white"
-                                >
-                                    <use xlinkHref={`#arrow-left`}></use>
-                                </svg>
-                            </div>
-                        </>
-                    )
-                }
-            </div>
-        )
-    }
+                    {
+                        isMobile ? null : (
+                            <>
+                                <div className={styles.swiperPrev} onClick={() => {
+                                    swiperRef.current?.slidePrev()
+                                }}>
+                                    <svg width={18.67} height={16.33} viewBox='0 0 28 28' fill='none' stroke="white"
+                                    >
+                                        <use xlinkHref={`#arrow-left`}></use>
+                                    </svg>
+                                </div>
+                                <div className={styles.swiperNext} onClick={() => {
+                                    swiperRef.current?.slideNext()
+                                }}>
+                                    <svg width={18.67} height={16.33} viewBox='0 0 28 28' fill='none' stroke="white"
+                                    >
+                                        <use xlinkHref={`#arrow-left`}></use>
+                                    </svg>
+                                </div>
+                            </>
+                        )
+                    }
+                </div>
+            ) : (null)}
+        </>
+    )
+    // }
 }
 
 export default DiscountTabs;
