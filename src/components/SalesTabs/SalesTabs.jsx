27a0fdda9cb36c9fcb260f3d05adf7b47d5c1425
Isomukhammad <s.isomuxammad@gmail.com>
2023-02-03 23:09:54 +0500

    feat: connecting to API

--- src/components/SalesTabs/SalesTabs.jsx
+++ src/components/SalesTabs/SalesTabs.jsx
@@ -1,6 +1,8 @@
 import Image from 'next/image';
 import { useEffect } from 'react';
+import useSWR from 'swr';
 import data from '../../data.json';
+import fetcher from '../../utils/fetcher';
 import getCountdown from '../../utils/getCoundown';
 import TimerTab from '../TimerTab/TimerTab';
 
@@ -8,23 +10,34 @@ import styles from './SalesTabs.module.scss'
 
 const SalesTabs = () => {
     const { sales } = data;
+    const { data: promotions, error: promotionsError, isValidating } = useSWR('/promotions?page=1&quantity=3', (url) => fetcher(url),
+        {
+            revalidateIfStale: false,
+            revalidateOnFocus: false,
+            revalidateOnReconnect: false,
+        }
+    );
 
-    return (
-        <div className={styles.salesTab}>
-            <h1>Акции от 110-volt</h1>
-            <div className={styles.tabs}>
-                {
-                    sales.map(item => (
-                        <div key={item.id} className={styles.tab}>
-                            <Image src={item.img} alt="" width="0" height="0" sizes="100vw" placeholder="blurDataURL" />
+    if (!isValidating) {
+        return (
+            <div className={styles.salesTab}>
+                <h1 className='font-bold text-[32px]'>Акции от 110-volt</h1>
+                <div className={styles.tabs}>
+                    {
+                        promotions.data.map(item => (
+                            <div key={item.id} className={styles.tab}>
+                                <Image src={item.img} alt={item.name} width="0" height="0" sizes="100vw" placeholder="blurDataURL" />
 
-                            <TimerTab deadline={item.deadline} />
-                        </div>
-                    ))
-                }
+                                <div className={styles.timer}>
+                                    <TimerTab start={item.start_at} end={item.end_at} />
+                                </div>
+                            </div>
+                        ))
+                    }
+                </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
 
 export default SalesTabs;
