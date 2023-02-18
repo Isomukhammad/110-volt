2c8989b5cbc0aaf5af53e647d52e35aabeb754fd
Isomukhammad <s.isomuxammad@gmail.com>
2023-02-18 21:16:29 +0500

    feat: optimizing code

--- src/utils/getCoundown.js
+++ src/utils/getCoundown.js
@@ -1,32 +1,25 @@
-const getCountdown = (time) => {
-    const left = {
-        days: null,
-        hours: null,
-        minutes: null,
-        seconds: null
-    }
-    const timestamp = new Date(time).getTime();
-    const current = Date.now();
-    const countdown = timestamp - current;
+export const getCountdown = (time = '') => {
+    const countdown = new Date(time).getTime() - Date.now();
 
     if (countdown >= 0) {
         const days = Math.floor(countdown / (1000 * 60 * 60 * 24));
         const hours = Math.floor((countdown % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
         const minutes = Math.floor((countdown % (1000 * 60 * 60)) / (1000 * 60));
         const seconds = Math.floor((countdown % (1000 * 60)) / 1000);
-        left.days = days;
-        left.hours = hours;
-        left.minutes = minutes;
-        left.seconds = seconds;
-    } else {
-        left.days = 0;
-        left.hours = 0;
-        left.minutes = 0;
-        left.seconds = 0;
+
+        return {
+            days,
+            hours,
+            minutes,
+            seconds,
+        }
     }
 
+    return {
+        days: 0,
+        hours: 0,
+        minutes: 0,
+        seconds: 0
+    }
 
-    return left;
 }
-
-export default getCountdown;
