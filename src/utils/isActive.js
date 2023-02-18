0eed3a55ab1ba2f4e016b164b55f4b7d9dc61342
Isomukhammad <s.isomuxammad@gmail.com>
2023-02-18 21:03:21 +0500

    refactor: optimizing code

--- src/utils/isActive.js
+++ src/utils/isActive.js
@@ -1,9 +1,5 @@
 export const isActive = ({ store, localStore, product }) => {
-    if (store) {
-        return store.items.some((item) => item.id === product.id) ? 1 : 0
-    } else if (localStore) {
-        return localStore.items.some((item) => item.id === product.id) ? 1 : 0
-    } else {
-        return 0;
-    }
+    if (!store || !product || !localStore) return 0;
+    if (store) return store.items.some((item) => item.id === product.id) ? 1 : 0;
+    return localStore.items.some((item) => item.id === product.id) ? 1 : 0;
 }
