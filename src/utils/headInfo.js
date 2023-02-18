26b09f3988a5e9e520dabc2306ed4451db97ab9e
Isomukhammad <s.isomuxammad@gmail.com>
2023-02-18 21:07:22 +0500

    refactor: optimizing code

--- src/utils/headInfo.js
+++ src/utils/headInfo.js
@@ -1,21 +1,13 @@
 import Head from "next/head";
 
-const HeadInfo = ({ title, description, keywords }) => {
-    return (
-        <Head>
-            <title>{title}</title>
-            <meta name="description" content={description} />
-            <meta name="keywords" content={keywords} />
-            <meta name="viewport" content="width=device-width, initial-scale=1" />
-            <link rel="icon" href="/favicon.ico" />
-        </Head>
-    )
-}
-
-HeadInfo.defaultProps = {
-    title: "110-volt.uz",
-    description: "Купить бытовую технику",
-    keywords: ""
-}
+const HeadInfo = ({ title = '110volt.uz', description = '', keywords = '' }) => (
+    <Head>
+        <title>{title}</title>
+        <meta name="description" content={description} />
+        <meta name="keywords" content={keywords} />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+    </Head>
+)
 
 export default HeadInfo;
