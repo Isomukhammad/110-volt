990db39ae22dfd8a771e03857cb1ee3ecb889320
Isomukhammad <s.isomuxammad@gmail.com>
2023-02-20 07:07:04 +0500

    refactor: file is deleted

--- src/utils/isActive.js
+++
@@ -1,5 +0,0 @@
-export const isActive = ({ store, localStore, product }) => {
-    if (!store || !product || !localStore) return 0;
-    if (store) return store.items.some((item) => item.id === product.id) ? 1 : 0;
-    return localStore.items.some((item) => item.id === product.id) ? 1 : 0;
-}
