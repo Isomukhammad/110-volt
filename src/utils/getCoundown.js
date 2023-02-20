ca11b478f30c687f37a20797b2f0f9d0e42e4a40
Isomukhammad <s.isomuxammad@gmail.com>
2023-02-20 07:08:01 +0500

    refactor: optimizing code

--- src/utils/getCoundown.js
+++ src/utils/getCoundown.js
@@ -1,25 +1,24 @@
 export const getCountdown = (time = '') => {
     const countdown = new Date(time).getTime() - Date.now();
 
-    if (countdown >= 0) {
-        const days = Math.floor(countdown / (1000 * 60 * 60 * 24));
-        const hours = Math.floor((countdown % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        const minutes = Math.floor((countdown % (1000 * 60 * 60)) / (1000 * 60));
-        const seconds = Math.floor((countdown % (1000 * 60)) / 1000);
-
+    if (countdown <= 0) {
         return {
-            days,
-            hours,
-            minutes,
-            seconds,
+            days: 0,
+            hours: 0,
+            minutes: 0,
+            seconds: 0
         }
     }
 
-    return {
-        days: 0,
-        hours: 0,
-        minutes: 0,
-        seconds: 0
-    }
+    const days = Math.floor(countdown / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((countdown % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((countdown % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((countdown % (1000 * 60)) / 1000);
 
+    return {
+        days,
+        hours,
+        minutes,
+        seconds,
+    }
 }
